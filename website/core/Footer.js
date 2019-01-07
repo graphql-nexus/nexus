b4d2447a5c41d618a0929e9ad3c399544cd42fdb
Tim Griesser <tgriesser10@gmail.com>
2019-01-06 21:11:09 -0500

    Fixing a few docs links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -45,7 +45,7 @@ class Footer extends React.Component {
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
+              data-count-href="/graphql-nexus/nexus/stargazers"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub"
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,6 +38,10 @@ const siteConfig = {
       href: "https://github.com/graphql-nexus/nexus/tree/develop/examples",
       label: "Examples",
     },
+    {
+      href: "https://github.com/graphql-nexus/nexus",
+      label: "GitHub",
+    },
   ],
 
   // If you have users set above, you add it here:
@@ -106,7 +110,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl: "https://github.com/graphql-nexus/nexus",
   separateCss: ["static/separate-css"],
 
   users,
