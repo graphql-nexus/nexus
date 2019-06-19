b1089a5665e5e3eaefdf1ffb4424119e1bb66828
Pascal Sthamer <sthamer.pascal@gmail.com>
2019-06-19 11:08:25 +0200

    Fix wrong fieldName type on subscriptionField (and fix backingTypes.spec.ts) (#159)
    * Fix wrong fieldName type on subscriptionField
    
    Closes #158
    
    * Try to fix backingTypes.spec.ts
    
    * Remove unused NexusSchemaExtensions import

--- tests/backingTypes.spec.ts
+++ tests/backingTypes.spec.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { core, makeSchema, queryType, enumType } from "../src";
 import { A, B } from "./_types";
-import { NexusSchemaExtensions } from "../src/core";
 
 const { Typegen, TypegenMetadata } = core;
 
@@ -46,7 +45,6 @@ function getSchemaWithConstEnums() {
 
 describe("backingTypes", () => {
   let metadata: core.TypegenMetadata;
-  let schemaExtensions: NexusSchemaExtensions;
 
   beforeEach(async () => {
     metadata = new TypegenMetadata({
