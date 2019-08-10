eea91ef6bf9b0ef19fce3b5e4965afdfdf8fb4b8
Jason Kuhrt <jasonkuhrt@me.com>
2019-08-10 15:28:56 -0400

    better jest ts error formatting

--- tests/_setup.ts
+++ tests/_setup.ts
@@ -1,14 +1,34 @@
 import ts from "typescript";
 
+(global as any).PROJECT_ROOT = "src/";
+
+const formatTSDiagonsticsForJest = (
+  diagnostics: readonly ts.Diagnostic[]
+): string => {
+  const formatHost: ts.FormatDiagnosticsHost = {
+    getCanonicalFileName: (path) => path,
+    getCurrentDirectory: ts.sys.getCurrentDirectory,
+    getNewLine: () => ts.sys.newLine,
+  };
+
+  const tsReport = ts.formatDiagnosticsWithColorAndContext(
+    diagnostics,
+    formatHost
+  );
+
+  const sourcePath =
+    process.cwd() + "/" + (((global as any).PROJECT_ROOT as string) || "");
+
+  const jestReport = `${diagnostics.length} Type Error(s):\n\n${diagnostics
+    .map((d) => `${d.file.fileName.replace(sourcePath, "")}`)
+    .join("\n")}\n\n${tsReport}`;
+
+  return jestReport;
+};
+
 expect.extend({
   toTypeCheck(fileNames: string[], config: ts.CompilerOptions) {
     const diagnostics = ts.createProgram(fileNames, config).emit().diagnostics;
-
-    const formatHost: ts.FormatDiagnosticsHost = {
-      getCanonicalFileName: (path) => path,
-      getCurrentDirectory: ts.sys.getCurrentDirectory,
-      getNewLine: () => ts.sys.newLine,
-    };
 
     const pass = diagnostics.length === 0;
 
@@ -16,7 +36,7 @@ expect.extend({
       message: () =>
         pass
           ? "expected program to not typecheck"
-          : ts.formatDiagnosticsWithColorAndContext(diagnostics, formatHost),
+          : formatTSDiagonsticsForJest(diagnostics),
       pass,
     };
   },
--- tests/integration/_helpers.ts
+++ tests/integration/_helpers.ts
@@ -3,7 +3,7 @@ import { generateSchema as doGenerateSchema } from "../../src/builder";
 import ts from "typescript";
 
 export const testSchema = (name: string) => {
-  it(`can be compiled with typegen from ${name} schema`, async () => {
+  it(`can compile ${name} app with its typegen`, async () => {
     const appFilePath = join(__dirname, `./_${name}.ts`);
     const typegenFilePath = `/tmp/nexus-integration-test-typegen-${name}-${Date.now()}.d.ts`;
     await doGenerateSchema({
