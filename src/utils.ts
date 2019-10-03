fec206c5fd7b220a6ed84eb826facbe3f8246c4b
Jason Kuhrt <jasonkuhrt@me.com>
2019-10-03 16:49:14 -0400

    remove utils edit

--- src/utils.ts
+++ src/utils.ts
@@ -236,10 +236,3 @@ export function relativePathTo(
   }
   return path.join(relative, filename);
 }
-
-/**
- * Index types are just an alias for Records whose keys are of type `string`.
- * The name of this type, `Index`, signifies its canonical use-case for data
- * indexed by some property, e.g. a list of users indexed by email.
- */
-export type Index<T> = Record<string, T>;
