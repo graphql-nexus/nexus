2b0ed408511d19946d211894d72e064835428df0
Tim Griesser <tgriesser10@gmail.com>
2019-10-10 10:28:54 -0400

    Skip nexus-prisma for tests, don't run in band in ci

--- tests/integration/_app.typegen.ts
+++ tests/integration/_app.typegen.ts
@@ -3,6 +3,7 @@
  * Do not make changes to this file directly
  */
 
+import { core } from "../..";
 declare global {
   interface NexusGenCustomInputMethods<TypeName extends string> {
     title(...args: any): void;
