d8bd4e10450945a874db663a926557f80629cc3a
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-02-04 12:33:21 -0600

    wrap authorization middleware both on onCreateFieldSubscribe and onCreateFieldResolver

--- src/plugins/fieldAuthorizePlugin.ts
+++ src/plugins/fieldAuthorizePlugin.ts
@@ -1,4 +1,4 @@
-import { plugin } from "../plugin";
+import { plugin, CreateFieldResolverInfo, MiddlewareFn } from "../plugin";
 import {
   RootValue,
   ArgsValue,
@@ -77,12 +77,85 @@ export const fieldAuthorizePlugin = (
     );
     throw new Error("Not authorized");
   };
+
   let hasWarned = false;
+
+  const authorizationMiddleware = (
+    config: CreateFieldResolverInfo<any, any>
+  ): MiddlewareFn | undefined => {
+    const authorize = config.fieldConfig.extensions?.nexus?.config.authorize;
+    // If the field doesn't have an authorize field, don't worry about wrapping the resolver
+    if (authorize == null) {
+      return;
+    }
+    // If it does have this field, but it's not a function, it's wrong - let's provide a warning
+    if (typeof authorize !== "function") {
+      console.error(
+        new Error(
+          `The authorize property provided to ${
+            config.fieldConfig.type
+          } should be a function, saw ${typeof authorize}`
+        )
+      );
+      return;
+    }
+    // If they have it, but didn't explicitly specify a plugins array, warn them.
+    if (
+      !config.schemaConfig.plugins?.find(
+        (p) => p.config.name === "NexusAuthorize"
+      )
+    ) {
+      if (!hasWarned) {
+        console.warn(
+          'The GraphQL Nexus "authorize" feature has been moved to a plugin, add [fieldAuthorizePlugin()] to your makeSchema plugin config to remove this warning.'
+        );
+        hasWarned = true;
+      }
+    }
+    // The authorize wrapping resolver.
+    return function(root, args, ctx, info, next) {
+      let toComplete;
+      try {
+        toComplete = authorize(root, args, ctx, info);
+      } catch (e) {
+        toComplete = Promise.reject(e);
+      }
+      return plugin.completeValue(
+        toComplete,
+        (authResult) => {
+          if (authResult === true) {
+            return next(root, args, ctx, info);
+          }
+          const finalFormatError = ensureError(root, args, ctx, info);
+          if (authResult instanceof Error) {
+            finalFormatError(authResult);
+          }
+          if (authResult === false) {
+            finalFormatError(new Error("Not authorized"));
+          }
+          const {
+            fieldName,
+            parentType: { name: parentTypeName },
+          } = info;
+          finalFormatError(
+            new Error(
+              `Nexus authorize for ${parentTypeName}.${fieldName} Expected a boolean or Error, saw ${authResult}`
+            )
+          );
+        },
+        (err) => {
+          ensureError(root, args, ctx, info)(err);
+        }
+      );
+    };
+  };
+
   return plugin({
     name: "NexusAuthorize",
     description:
       "The authorize plugin provides field-level authorization for a schema.",
     fieldDefTypes: fieldDefTypes,
+<<<<<<< HEAD
     onCreateFieldResolver(config) {
       const authorize = config.fieldConfig.extensions?.nexus?.config.authorize;
       // If the field doesn't have an authorize field, don't worry about wrapping the resolver
@@ -152,5 +225,9 @@ export const fieldAuthorizePlugin = (
         );
       };
     },
+=======
+    onCreateFieldSubscribe: authorizationMiddleware,
+    onCreateFieldResolver: authorizationMiddleware,
+>>>>>>> wrap authorization middleware both on onCreateFieldSubscribe and onCreateFieldResolver
   });
 };
