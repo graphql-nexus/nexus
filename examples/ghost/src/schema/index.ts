6d68d4eba0da18b1a1525445084c6de34510286f
Jason Kuhrt <jasonkuhrt@me.com>
2020-08-07 16:02:20 -0400

    update examples

--- examples/ghost/src/schema/index.ts
+++ examples/ghost/src/schema/index.ts
@@ -1,11 +1,11 @@
-import { decorateType } from "@nexus/schema";
-import { GraphQLDate } from "graphql-iso-date";
+import { decorateType } from '@nexus/schema'
+import { GraphQLDate } from 'graphql-scalars'
 
 export const GQLDate = decorateType(GraphQLDate, {
-  rootTyping: "Date",
-  asNexusMethod: "date",
-});
+  rootTyping: 'Date',
+  asNexusMethod: 'date',
+})
 
-export * from "./User";
-export * from "./Query";
-export * from "./Post";
+export * from './Post'
+export * from './Query'
+export * from './User'
