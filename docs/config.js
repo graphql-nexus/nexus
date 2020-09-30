796add7c0cf446071e48d055c3fe447b37545c09
Jan Piotrowski <piotrowski+github@gmail.com>
2020-09-30 13:07:48 +0200

    fix: Github link

--- docs/config.js
+++ docs/config.js
@@ -38,7 +38,7 @@ const config = {
     title: 'Nexus - title',
     description: 'Nexus - desc',
     keywords: 'Docs, nexus, 1.0',
-    docsLocation: 'https://github.com/graphql-nexus/schema/tree/develop/docs',
+    docsLocation: 'https://github.com/graphql-nexus/schema/tree/develop/docs/content',
     twitter: {
       site: '@nexusgql',
       creator: '@nexusgql',
