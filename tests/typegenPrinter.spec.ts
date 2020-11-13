f1bc2d78b4c38d2450796314389fbf94b12ae78c
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-11-13 20:17:40 +0100

    small fix

--- tests/typegenPrinter.spec.ts
+++ tests/typegenPrinter.spec.ts
@@ -41,7 +41,6 @@ describe('typegenPrinter', () => {
         ],
         contextType: 't.TestContext',
       },
-      nexusSchemaImportId: '../../src',
     })
     // give time for artifact generation to complete
     await new Promise((res) => setTimeout(res, 2000))
