1499939e226ca8c4ebe63da7b791a437523f216d
Tim Griesser <tgriesser10@gmail.com>
2020-11-21 16:56:36 -0500

    Fix for windows?

--- tests/__helpers/sequencer.js
+++ tests/__helpers/sequencer.js
@@ -1,4 +1,5 @@
 // testSequencer.js
+const path = require('path')
 const Sequencer = require('@jest/test-sequencer').default
 
 class NexusSequencer extends Sequencer {
@@ -6,7 +7,9 @@ class NexusSequencer extends Sequencer {
     // Test structure information
     // https://github.com/facebook/jest/blob/6b8b1404a1d9254e7d5d90a8934087a9c9899dab/packages/jest-runner/src/types.ts#L17-L21
     const copyTests = Array.from(tests)
-    return copyTests.sort((testA, testB) => (testA.path.includes('tests/integrations') ? 1 : -1))
+    return copyTests.sort((testA, testB) =>
+      testA.path.includes(path.join('tests', 'integrations')) ? 1 : -1
+    )
   }
 }
 
