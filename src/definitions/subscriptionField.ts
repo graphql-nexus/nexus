d5aeb7660562817ab6d5e408b8871d8470e35f4d
Jason Kuhrt <jasonkuhrt@me.com>
2020-12-02 09:50:24 -0500

    fix: subscriptionField overload for builder api access
    closes #595

--- src/definitions/subscriptionField.ts
+++ src/definitions/subscriptionField.ts
@@ -1,16 +1,31 @@
-import { extendType } from './extendType'
-import { SubscriptionTypeConfig } from './subscriptionType'
+import { extendType, NexusExtendTypeDef } from './extendType'
+import { SubscriptionBuilder, SubscriptionTypeConfig } from './subscriptionType'
+
+export type SubscriptionFieldConfig<FieldName extends string, Event> =
+  | SubscriptionTypeConfig<FieldName, Event>
+  | (() => SubscriptionTypeConfig<FieldName, Event>)
+
+export function subscriptionField(
+  fieldFn: (t: SubscriptionBuilder) => void
+): NexusExtendTypeDef<'Subscription'>
+
+export function subscriptionField<FieldName extends string, Event>(
+  fieldName: FieldName,
+  config: SubscriptionFieldConfig<FieldName, Event>
+): NexusExtendTypeDef<'Subscription'>
 
 /**
  * Add one field to the Subscription type
  */
-export function subscriptionField<FieldName extends string, Event>(
-  fieldName: FieldName,
-  config: SubscriptionTypeConfig<FieldName, Event> | (() => SubscriptionTypeConfig<FieldName, Event>)
-) {
+export function subscriptionField(...args: any[]) {
   return extendType({
     type: 'Subscription',
     definition(t) {
+      if (typeof args[0] === 'function') {
+        return args[0](t)
+      }
+
+      const [fieldName, config] = args as [string, SubscriptionFieldConfig<any, any>]
       const finalConfig = typeof config === 'function' ? config() : config
       t.field(fieldName, finalConfig as any)
     },
