3f14aa65840683ea57ae3d6bccb04f350e38add8
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-12-04 15:21:32 +0100

    chore: rename notice

---
+++ scripts/rename-notice.js
@@ -0,0 +1,14 @@
+const ansi = {
+  bold: (str) => `\u001b[1m${str}${ansi.reset}`,
+  yellow: (str) => `\u001b[33m${str}${ansi.reset}`,
+  reset: '\u001b[0m',
+}
+
+process.stdout.write(`\
+${ansi.yellow(ansi.bold("WARNING: '@nexus/schema' will be renamed to 'nexus' on December 14th."))}
+${ansi.yellow(ansi.bold("WARNING: '@nexus/schema' will be renamed to 'nexus' on December 14th."))}
+${ansi.yellow(ansi.bold("WARNING: '@nexus/schema' will be renamed to 'nexus' on December 14th."))}
+
+0.20.x will be the last versions published on the '@nexus/schema' package.
+Future updates will be published to 'nexus@1.0.0'.
+`)
