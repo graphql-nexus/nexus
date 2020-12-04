2c8102b37eb6b17adca0ab0539a91bf3f1ecab82
Jason Kuhrt <jason.kuhrt@dialogue.co>
2020-12-04 13:53:57 -0500

    docs: cover objectType with jsdocs (#703)

--- src/definitions/objectType.ts
+++ src/definitions/objectType.ts
@@ -1,8 +1,8 @@
 import { assertValidName } from 'graphql'
 import { InterfaceFieldsFor } from '../typegenTypeHelpers'
-import { AbstractTypes, NexusTypes, NonNullConfig, Omit, RootTypingDef, withNexusSymbol } from './_types'
 import { OutputDefinitionBlock, OutputDefinitionBuilder } from './definitionBlocks'
-import { Implemented, FieldModificationDef, FieldModification } from './interfaceType'
+import { FieldModification, FieldModificationDef, Implemented } from './interfaceType'
+import { AbstractTypes, NexusTypes, NonNullConfig, RootTypingDef, withNexusSymbol } from './_types'
 
 export interface ObjectDefinitionBuilder extends OutputDefinitionBuilder {
   addInterfaces(toAdd: Implemented[]): void
@@ -13,15 +13,11 @@ export class ObjectDefinitionBlock<TypeName extends string> extends OutputDefini
   constructor(protected typeBuilder: ObjectDefinitionBuilder) {
     super(typeBuilder)
   }
-  /**
-   * @param interfaceName
-   */
+  /** @param interfaceName */
   implements(...interfaceName: Array<Implemented>) {
     this.typeBuilder.addInterfaces(interfaceName)
   }
-  /**
-   * Modifies a field added via an interface
-   */
+  /** Modifies a field added via an interface */
   modify<FieldName extends Extract<InterfaceFieldsFor<TypeName>, string>>(
     field: FieldName,
     modifications: FieldModification<TypeName, FieldName>
@@ -30,24 +26,141 @@ export class ObjectDefinitionBlock<TypeName extends string> extends OutputDefini
   }
 }
 
+/** Configuration object to specify an object's name, fields, and more. */
 export type NexusObjectTypeConfig<TypeName extends string> = {
-  name: TypeName
-  definition(t: ObjectDefinitionBlock<TypeName>): void
   /**
-   * Configures the nullability for the type, check the
-   * documentation's "Getting Started" section to learn
-   * more about GraphQL Nexus's assumptions and configuration
-   * on nullability.
+   * [GraphQL 2018 Spec](https://spec.graphql.org/June2018/#Name)
+   *
+   * The name of this object type. Must conform to the pattern:
+   *
+   * [_A-Za-z][_0-9A-Za-z]*
+   *
+   * From the spec: Names in GraphQL are case‐sensitive. That is to say name, Name, and NAME all refer to
+   * dfferent names. Underscores are significant, which means other_name and othername are two differen names.
+   *
+   * @example
+   *   'Post'
+   *
+   * @example
+   *   'User2'
+   *
+   * @example
+   *   'comment'
+   *
+   * @example
+   *   'User_Timeline'
+   */
+  name: TypeName
+  /**
+   * [Nexus Nullability Guide](https://nxs.li/guides/nullability)
+   *
+   * Configures the default nullability for fields and arguments in this object.
+   *
+   * Default :: By default inherits from the global nonNullDefaults configuration found in makeSchema.
+   *
+   * @example
+   *   const User = objectType({
+   *     name: 'User',
+   *     nonNullDefaults: {
+   *       input: true, // field args, input object type fields
+   *       output: false, // object type fields
+   *     },
+   *     definition(t) {
+   *       t.string('location', {
+   *         args: {
+   *           language: stringArg(),
+   *         },
+   *       })
+   *     },
+   *   })
+   *
+   *   // GraphQL SDL
+   *   // -----------
+   *   //
+   *   // type User {
+   *   //   location(language: String!): String
+   *   // }
    */
   nonNullDefaults?: NonNullConfig
   /**
-   * The description to annotate the GraphQL SDL
+   * [GraphQL 2018 Spec](https://spec.graphql.org/June2018/#sec-Descriptions)
+   *
+   * The description for this object type.
+   *
+   * Various GraphQL tools will make use of this information but it has zero runtime impact. The value given
+   * here will also be included as heredocs in the generated GraphQL SDL file.
+   *
+   * Default :: By default there will be no description
+   *
+   * @example
+   *   'Little description to help you along!'
+   *
+   *   // GraphQL SDL
+   *   // -----------
+   *   // """Little description to help you along!"""
+   *   // type YOUR_TYPE_NAME {
+   *   //   # ...
+   *   // }
    */
   description?: string | null
   /**
-   * Root type information for this type
+   * [Nexus Backing Types Guide](https://nxs.li/guides/backing-types)
+   *
+   * Specify the backing type for this object type.
+   *
+   * You can give a literal TypeScript type written as a string or give the location of a module that exports a type.
+   *
+   * Default :: By default the backing type of this object will be whatever you configured in makeSchema, if
+   * anything, otherwise simply 1:1 with how you have defined this object type.
+   *
+   * @example
+   *   '{ foo: string; qux: number; bar: boolean }'
+   *
+   * @example
+   *   {
+   *   path: 'some-package',
+   *   name: 'User',
+   *   }
+   *
+   * @example
+   *   {
+   *   path: `${__dirname}/some/module.ts`,
+   *   name: 'User',
+   *   }
    */
   rootTyping?: RootTypingDef
+  /**
+   * Define the fields of your object type.
+   *
+   * This method receives a type builder api that you will use to define the fields of your object type
+   * within. You can leverage conditionals, loops, other functions (that take the builder api as an
+   * argument), pull in variables from higher sopes, and so on, to help define your fields. However avoid two things:
+   *
+   * 1. Doing asyncrous work when defining fields.
+   *
+   * 2. Triggering side-effects that you would NOT want run at _build_ time––as this code will run during
+   * build to support [Nexus' reflection system](https://nxs.li/guides/reflection).
+   *
+   * @example
+   *   objectType({
+   *     name: 'User',
+   *     definition(t) {
+   *       t.field('name', { type: 'String' })
+   *       t.string('status')
+   *       t.list.list.int('foo')
+   *       t.nullable.boolean('visible')
+   *       t.list.nonNull.field('friends', {
+   *         type: 'Friend',
+   *         // ...
+   *       })
+   *     },
+   *   })
+   *
+   * @param t The type builder API for object types. The primary method you'll find is "t.field" but there are
+   *     many convenient shorthands available as well, plus anything plugins have added. Explore each one's jsDoc
+   *     for more detail.
+   */
+  definition(t: ObjectDefinitionBlock<TypeName>): void
 } & AbstractTypes.MaybeTypeDefConfigFieldIsTypeOf<TypeName> &
   NexusGenPluginTypeConfig<TypeName>
 
@@ -62,14 +175,27 @@ export class NexusObjectTypeDef<TypeName extends string> {
 
 withNexusSymbol(NexusObjectTypeDef, NexusTypes.Object)
 
+/**
+ * [Nexus Docs](https://nxs.li/docs/api/object-type) | [GraphQL.org
+ * Docs](https://graphql.org/learn/schema/#object-types-and-fields) | [GraphQL 2018
+ * Spec](https://spec.graphql.org/June2018/#sec-Objects)
+ *
+ * Define a GraphQL Object Type.
+ *
+ * Object types are typically the most common kind of type present in a GraphQL schema. You give them a name
+ * and filds that model your domain. Fields are typed and can point to yet another object type you've defined.
+ *
+ * @example
+ *   const Post = objectType({
+ *     name: 'Post',
+ *     definition(t) {
+ *       t.int('id')
+ *       t.string('title')
+ *     },
+ *   })
+ *
+ * @param config Specify your object's name, its fields, and more. See each config property's jsDoc for more detail.
+ */
 export function objectType<TypeName extends string>(config: NexusObjectTypeConfig<TypeName>) {
   return new NexusObjectTypeDef<TypeName>(config.name, config)
-}
-
-export function queryType(config: Omit<NexusObjectTypeConfig<'Query'>, 'name'>) {
-  return objectType({ ...config, name: 'Query' })
-}
-
-export function mutationType(config: Omit<NexusObjectTypeConfig<'Mutation'>, 'name'>) {
-  return objectType({ ...config, name: 'Mutation' })
 }
