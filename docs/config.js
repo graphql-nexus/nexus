0ed742a8a5e75e9ef591fafd3191d1b03e03fcc7
Jason Kuhrt <jasonkuhrt@me.com>
2020-12-16 09:06:39 -0500

    chore(docs): fix URLs point to git repo trunk

--- docs/config.js
+++ docs/config.js
@@ -38,7 +38,7 @@ const config = {
     title: 'Nexus - title',
     description: 'Nexus - desc',
     keywords: 'Docs, nexus, 1.0',
-    docsLocation: 'https://github.com/graphql-nexus/nexus/tree/develop/docs/content',
+    docsLocation: 'https://github.com/graphql-nexus/nexus/tree/main/docs/content',
     twitter: {
       site: '@nexusgql',
       creator: '@nexusgql',
