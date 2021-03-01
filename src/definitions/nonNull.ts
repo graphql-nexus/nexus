2ac44324cde9d03bf0dcd1921f65d78e3c800022
Tim Griesser <tgriesser10@gmail.com>
2021-03-01 11:37:56 -0500

    fix: cast NexusNonNullDef to prevent union type complexity error

--- src/definitions/nonNull.ts
+++ src/definitions/nonNull.ts
@@ -67,7 +67,7 @@ withNexusSymbol(NexusNonNullDef, NexusTypes.NonNull)
  */
 export function nonNull<TypeName extends NexusNonNullableTypes>(type: TypeName) {
   if (isNexusNonNullTypeDef(type) || isNonNullType(type)) {
-    return type
+    return type as NexusNonNullDef<any>
   }
   if (isNexusNullTypeDef(type)) {
     return new NexusNonNullDef(type.ofNexusType)
