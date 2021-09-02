d4c626077465bd012a22840d1a14d47c9a0eb5c0
Tim Griesser <tgriesser10@gmail.com>
2021-09-02 09:38:16 -0400

    feat: ability to rename root types (#976)
    Closes #867

---
+++ tests/builder.spec.ts
@@ -0,0 +1,76 @@
+import { lexicographicSortSchema, printSchema } from 'graphql'
+import { makeSchema, objectType } from '../src'
+
+describe('builder', () => {
+  it('can replace the Query root type with an alternate type', () => {
+    const OtherQuery = objectType({
+      name: 'RootQuery',
+      definition(t) {
+        t.string('name')
+      },
+    })
+
+    const Query = objectType({
+      name: 'Query',
+      definition(t) {
+        t.string('ok')
+      },
+    })
+
+    const schema = makeSchema({
+      types: [OtherQuery, Query],
+      schemaRoots: {
+        query: OtherQuery,
+      },
+    })
+    expect(printSchema(lexicographicSortSchema(schema))).toMatchSnapshot()
+  })
+
+  it('can replace the Mutation root type with an alternate type', () => {
+    const OtherMutation = objectType({
+      name: 'RootMutation',
+      definition(t) {
+        t.string('name')
+      },
+    })
+
+    const Mutation = objectType({
+      name: 'Mutation',
+      definition(t) {
+        t.string('ok')
+      },
+    })
+
+    const schema = makeSchema({
+      types: [OtherMutation, Mutation],
+      schemaRoots: {
+        mutation: OtherMutation,
+      },
+    })
+    expect(printSchema(lexicographicSortSchema(schema))).toMatchSnapshot()
+  })
+
+  it('can replace the Subscription root type with an alternate type', () => {
+    const OtherSubscription = objectType({
+      name: 'RootSubscription',
+      definition(t) {
+        t.string('name')
+      },
+    })
+
+    const Subscription = objectType({
+      name: 'Subscription',
+      definition(t) {
+        t.string('ok')
+      },
+    })
+
+    const schema = makeSchema({
+      types: [OtherSubscription, Subscription],
+      schemaRoots: {
+        subscription: OtherSubscription,
+      },
+    })
+    expect(printSchema(lexicographicSortSchema(schema))).toMatchSnapshot()
+  })
+})
